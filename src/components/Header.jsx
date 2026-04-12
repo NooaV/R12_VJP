315b22420f2b7c5ee1d66cb6a65ee0b0bf67515d
NooaV <nooa.virtala@aalto.fi>
2026-04-12 19:14:57 +0300

    alkuvaiheen Header ja siihen navbar tehty

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import Navbar from "./Navbar";
+import "./Header.css"
+import HSL_logo from "../assets/HSL_logo.jpg";
+
+
+function Header() {
+    return (
+        <header className ="header">
+            
+            <div className="logo">
+                <img src={HSL_logo} alt="HSL logo" className="logo-icon"/>
+            </div>
+
+            <Navbar />
+
+            <div className="info">
+                <span>i{/*tähän info logo*/}</span>
+            </div>
+
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+
+function Navbar() {
+return (
+    <nav className ="nav">
+        <a href="#">Pelit</a>
+        <a href="#">Faktatieto</a>
+        <a href="#">Träkkeri</a>
+        <a href="#">Meditaatio</a>
+    </nav>
+    );
+}
+
+export default Navbar;
