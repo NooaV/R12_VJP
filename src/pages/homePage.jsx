5aa02bbe36d67735e9abc4e418fc89987e6266b4
NooaV <nooa.virtala@aalto.fi>
2026-05-03 22:01:33 +0300

    muutin ton väliaikasen  ohjetekstin viel accurateks

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -8,7 +8,7 @@ import Tekemiset from "../components/tekemiset/tekemiset";
 function Home() {
   return (
     <>
-      <p>Kaikki sisältö tänne mainin sisään! pages/homePage.jsx!</p>
+      <p>Kaikki sisältö tänne eli esim Hero osion react viittaus tekemiset ylös vaan siistiin sarakkeeseen! etc. pages/homePage.jsx!</p>
       <Tekemiset />
     </>
   );
