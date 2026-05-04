7b2b5081a4888c5c7e36a9b09c4cbac5ebbbdcda
lumiahe1 <elias.lumiaho@aalto.fi>
2026-05-04 15:43:16 +0300

    vähennä käyttöä niin ettei valu reunapalkin päälle

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import Header from "./components/navigation/Header";
 import Footer from "./components/navigation/Footer";
 import Tekemiset from "./components/tekemiset/tekemiset";
+import Hero from "./components/hero/hero";
 
 import Home from "./pages/homePage";
 import FaktaTieto from "./pages/faktaTietoPage";
@@ -16,7 +17,7 @@ function App() {
  
     <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh"}}>
       <Header />
-
+      <Hero />
       <main style ={{flex: 1}}>
         <Routes>
           <Route path ="/" element={<Home />} />
--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,4 +1,31 @@
-import "./Hero.css";
+import React from 'react';
+import './hero.css';
+
+// const Hero = () => {
+//   return (
+//     <section className="hero-container">
+//       <div className="static-underlay-container">
+//         <div className="static-noise"></div>
+//       </div>
+
+//       <div 
+//         className="hero-foreground"
+//         style={{ backgroundImage: `url(${'src/assets/photos/hero_osio.png'})` }}
+//       ></div>
+
+//       <div className="hero-content">
+//         <h1 className="hero-title">
+//           Katse<br />ylös
+//         </h1>
+//       </div>
+
+//       <div className="hero-logo-container">
+//         <div className="logo-placeholder"></div>
+//       </div>
+//     </section>
+//   );
+// };
+
 
 export default function Hero() {
   return <section className="hero"></section>;
