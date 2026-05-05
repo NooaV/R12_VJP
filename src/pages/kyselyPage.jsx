c13611d9a25130a36c7e04c30d6244bc27eeac97
eemil_lou <eemil.lounila@aalto.fi>
2026-05-05 13:27:26 +0300

    kyselysivu luotu

---
+++ src/pages/kyselyPage.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/navigation/Header";
+import Footer from "./components/navigation/Footer";
+import Kysely from "../components/kysely/kysely";
+
+
+function Quiz() {
+  return (
+    <>
+      <Header />
+      <Kysely />
+      <Footer />
+
+    </>
+  );
+}
+
+export default Quiz;
