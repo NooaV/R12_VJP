a14fa4bae76900b6d89d47bbf90438c7dd2bcbdd
philiphermunen <philiphermunen@gmail.com>
2026-05-06 13:24:43 +0300

    Herotop lisäys

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -4,12 +4,14 @@ import Tekemiset from "../components/tekemiset/tekemiset";
 import Hero from "../components/hero/hero";
 import Vahennakayttoa from "../components/vahennakayttoa/vahennakayttoa";
 import TestiSection from "../components/testi/testiSection";
+import HeroTop from "../components/herotop/herotop";
 
 
 function Home() {
   return (
     <>
       {/* <p>Kaikki sisältö tänne eli esim Hero osion react viittaus tekemiset ylös vaan siistiin sarakkeeseen! etc. pages/homePage.jsx!</p> */}
+      <HeroTop />
       <Vahennakayttoa />
       <TestiSection />
       <Tekemiset />
