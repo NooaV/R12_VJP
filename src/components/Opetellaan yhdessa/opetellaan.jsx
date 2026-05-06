49bd8711cded0bebd1a9f99a3f41e992eadbed97
Aino Koivukangas <aino.koivukangas@aalto.fi>
2026-05-06 18:14:33 +0300

    Opetellaan-osion tyylit lisätty

--- src/components/Opetellaan yhdessa/opetellaan.jsx
+++ src/components/Opetellaan yhdessa/opetellaan.jsx
@@ -1,22 +1,24 @@
 //Ainon osio
 import kuva1 from "/public/images/juna.jpg";
-import kuva2 from "/public/images/Kate_ylös.jpg";
-import kuva3 from "/public/images/metro.JPG";
+import kuva3 from "/public/images/Kate_ylös.jpg";
+import kuva2 from "/public/images/metro.JPG";
 import "./opetellaan.css";
 
 export default function OpetellaanYhdessa() {
   return (
     <section className= "opetellaan">
-      <h2 className= "opetellaan-title">Opetellaan yhdessä pitämään taukoa: katsotaan ylös!</h2>
+      <h2 className= "opetellaan-title"
+        >Opetellaan yhdessä pitämään taukoa: katsotaan ylös!</h2>
       <div className= "opetellaan-pics">
-      <img src={kuva1} alt="kuvituskuva1" />
-      <div className= "left-pics">
-        <img src={kuva2} alt="kuvituskva2" />
-        <img src={kuva3} alt="kuvituskuva3" />
+        <div className= "left-pics">
+          <img className = "pic" src={kuva1} alt="kuvituskva2" />
+          <img className = "pic" src={kuva2} alt="kuvituskuva3" />
+        </div>
+        <div className = "right-pic">
+          <img className= "pic2"src={kuva3} alt="kuvituskuva1" />
+        </div>
+        
       </div>
-      </div>
-
-
     </section>
 
     );
