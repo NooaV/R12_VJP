101a74ef61e0875c9452a6339ec0f10cb46dc4d1
Aino Koivukangas <aino.koivukangas@aalto.fi>
2026-05-08 17:49:35 +0300

    Mobiilinavigaation toteutus

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react"
 import { Link } from "react-router-dom";
 import { useLocation } from 'react-router-dom';
 
@@ -8,22 +9,40 @@ function Navbar() {
 
     const location = useLocation();
 
-    // 2. Määritetään, ollaanko etusivulla
+    //Määritetään, ollaanko etusivulla
     const isHomePage = location.pathname === "/";
 
-    return (
-        <nav className ="nav">
-            {!isHomePage && (
-                <Link to="/" className="nav-home-link">Etusivulle</Link>
-            )}
-            <Link to="/kysely">Pelit</Link>
-            <Link to="/faktatieto">Tietoa</Link>
+    //Mobiilinavigaation hallinta
+    const [isOpen, setOpen] = useState(false);
 
-           
-            
-            
+    const menuToggle = () => {setOpen(!isOpen);
+    }
+
+    const menuClose = () => {setOpen(false);
+    }
+
+
+    return (
+        //Mobiilinavigaation hampurilaisnappi ja avautuva navigointi
+        <nav className ="nav">
+            <div className={isOpen ? "burgerikuva open" : "burgerikuva"} onClick={menuToggle}>
+                <div className="bar"></div>
+                <div className="bar"></div>
+                <div className="bar"></div>
+            </div>
+        
+            <div className={isOpen ? "nav-items open" : "nav-items"}>
+                <ul className ="nav-list">
+                    {!isHomePage && (
+                        <li><Link to="/" className="nav-home-link" onClick={menuClose}>Etusivulle</Link></li>
+                    )}
+                    <li><Link to="/kysely" onClick={menuClose}>Pelit</Link></li>
+                    <li><Link to="/faktatieto" onClick={menuClose}>Tietoa</Link></li>
+                </ul>
+            </div>     
         </nav>
     );
 }
 
 export default Navbar;
+
